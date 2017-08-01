0a2f3c609d4f45f8d6dde6b3dd64e97139da5ddf
Jared Henderson <jared@netrivet.com>
2017-08-01 18:20:00 -0400

    add app.css with bootstrap grid

--- views/head.php
+++ views/head.php
@@ -6,5 +6,6 @@
         <title>Friends Library</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/app.css" media="all" rel="stylesheet" />
     </head>
     <body>
