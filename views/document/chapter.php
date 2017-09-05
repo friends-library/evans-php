908cac4ee75c8e96fac7b9882ff1545cacf4021f
Jared Henderson <jaredh159@users.noreply.github.com>
2017-09-05 19:47:36 -0400

    display edition chapters on document page (#22)

---
+++ views/document/chapter.php
@@ -0,0 +1,10 @@
+<h5><?= $chapter->getTitle() ?></h5>
+<ul class="chapter-formats">
+    <?php foreach ($chapter->getFormats() as $format) : ?>
+        <li class="chapter-formats__format">
+            <a href="<?= url($format, $chapter) ?>">
+                <?= $format->getType() ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
